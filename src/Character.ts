8c191f512bb37049dfe960339d1a074938241116
Érick Fernando <erick.f.rodrigues0@gmail.com>
2026-08-14 21:44:15 -0300

    trabaio

---
+++ src/Character.ts
@@ -0,0 +1,55 @@
+export class Character{
+    private name: string;
+    private level: number;
+    private health: number;
+
+    constructor(name: string, level: number, health: number){
+        this.name = name;
+        this.level = level;
+        this.health = health;
+    }
+
+    getName(): string{
+        return this.name;
+
+    }
+
+    setName(newName: string): void{
+        this.name = newName;
+    }
+
+    getLevel(): number{
+        return this.level;
+
+    }
+
+    getHealth(): number{
+        return this.health;
+    }
+
+    setHealth(newHealth: number): void{
+        this.health = newHealth;
+
+    }
+
+    takeDamage(amount: number): void{
+        if(this.health <= 0){
+            console.log(`O personagem não pode morrer!`);
+        } else{
+            this.health -= amount;
+
+        }
+        
+    }
+
+    showInfo(): void{
+        console.log(`
+            ==================
+            INFO CHARACTER
+            ==================
+            Name: ${this.name}
+            Level: ${this.level}
+            Health: ${this.health}`)
+
+    }
+}
